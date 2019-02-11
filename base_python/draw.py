0e70ca9c94db4841aaf04dfc44dc47922eff730f
unknown <ijon@stuy.edu>
2019-02-10 22:12:26 -0500

    attempt

--- base_python/draw.py
+++ base_python/draw.py
@@ -4,25 +4,13 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     deltaY = y1-y0
     deltaX = x1-x0
 
-    #If slope is undefined
+    #If line is vertical
     if(deltaX == 0 and deltaY != 0):
         for i in range(y0, y1):
             plot(screen, color, x0, i)
         return
 
     m = deltaY/deltaX
-
-    # #If slope is 1
-    # if(m == 1):
-    #     for i in range(x0, x1):
-    #         plot(screen, color, i,i)
-    #     return
-    #
-    # #If slope is -1
-    # if(m == -1):
-    #     for i in range(x1, x0 ):
-    #         plot(screen, color, i,i)
-    #     return
 
     #Octant 1
     if(m >= 0 and m <= 1):
@@ -57,18 +45,18 @@ def draw_line( x0, y0, x1, y1, screen, color ):
         return
 
     #Octant 7
-    if(m >= -1):
+    if(m <= -1):
         x = x0
         y = y0
         a = y1-y0
         b = -(x1-x0)
-        d = a - 2*b
+        d = a + 2*b
         while y >= y1:
             plot(screen, color, x, y)
-            if d < 0:
+            if d > 0:
                 x += 1
                 d += 2*a
-            y += 1
+            y -= 1
             d -= 2*b
         return
 
--- base_python/main.py
+++ base_python/main.py
@@ -4,22 +4,22 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-#Vertical line
-draw_line(0,0,0,500,screen,color)
-
-#Horizontal line
-draw_line(0,0,500,0,screen,color)
-
-#Line with slope 1
-draw_line(0,0,500,500,screen,color)
-
-#Octant 1 line
-draw_line(0,0,500,300,screen,color)
-
-#Octant 2 line
-draw_line(0,0,250,500,screen,color)
-
-#Octant 7 line
+# #Vertical line
+# draw_line(0,0,0,500,screen,color)
+#
+# #Horizontal line
+# draw_line(0,0,500,0,screen,color)
+#
+# #Line with slope 1
+# draw_line(0,0,500,500,screen,color)
+#
+# #Octant 1 line
+# draw_line(0,0,500,300,screen,color)
+#
+# #Octant 2 line
+# draw_line(0,0,250,500,screen,color)
+#
+# #Octant 7 line
 draw_line(0,500,500,0,screen,color)
 
 #Octant 8 line
